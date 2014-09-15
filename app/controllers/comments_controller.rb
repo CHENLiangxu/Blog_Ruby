8c6b84e0e3c9f5bbe7d4171d6b285de6b01d8c31
CHENLiangxu <chenliangxu68@gmail.com>
2014-09-15 19:51:00 +0000

    add authenticate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @post, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
